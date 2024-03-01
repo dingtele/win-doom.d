82b6a31f9e5bb9a73dcce4b7d4d1e92e111d50a7
dingtele <sardine323@gmail.com>
2024-03-01 17:38:09 +0800

    0301

--- custom.el
+++ custom.el
@@ -4,12 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vdiff-magit youdao-dictionary xwwp-follow-link-ivy toc-org telega setup racket-mode org-modern org-make-toc olivetti ivy gnu-elpa-keyring-update git geiser-racket fanyi exec-path-from-shell drag-stuff dimmer curl-to-elisp company clippy clipetty bing-dict anki-editor @)))
+   '(git-commit swiper vdiff-magit youdao-dictionary xwwp-follow-link-ivy toc-org telega setup racket-mode org-modern org-make-toc olivetti ivy gnu-elpa-keyring-update git geiser-racket fanyi exec-path-from-shell drag-stuff dimmer curl-to-elisp company clippy clipetty bing-dict anki-editor @)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-mode ((t (:family "UbuntuMono Nerd Font Mono" :height 1.2)))))
 (put 'set-goal-column 'disabled nil)
 (put 'customize-variable 'disabled nil)
